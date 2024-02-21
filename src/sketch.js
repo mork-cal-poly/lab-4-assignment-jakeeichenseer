5780c5783e525e2e8f679c1efad1624223e45889
Kirsten Mork <Jake Eichenseer>
2024-02-20 16:49:15 -0800

    sketch.js: pasted the creature code from lab 1

--- src/sketch.js
+++ src/sketch.js
@@ -10,3 +10,67 @@ function setup() {
 function draw() {
   background(220);
 }
+
+function drawCreature (x, y, colorBody, colorBodyFin) {
+  
+  push();
+      
+      translate(x, y); 
+  
+// Back Fin
+  
+  fill(240, 227, 0)
+  noStroke()
+  triangle(-175, -150, -100, -100, -175, -50)
+  
+// Stripe on Back Fin
+  
+  fill(0, 89, 240)
+  rect(-175, -105, 70, 10)
+ 
+// Bottom Right Fin
+  
+  fill(240, 138, 67)
+  triangle(0, -70, 100, -70, 0, 0)
+  
+// Bottom Left Fin
+  
+  fill(240, 198, 0)
+  triangle(-115, -100, 0, -40, -65, -7)
+  
+// Top Right Fin
+  
+  fill(240, 158, 45)
+  triangle(70, -165, 110, -110, 0, -110)
+  
+// Top Middle Fin
+  
+  fill(240, 198, 0)
+  triangle(0, -200, 110, -110, -30, -110)
+  
+// Top Left Fin
+  
+  fill(240, 138, 67)
+  triangle(-100, -205, 80, -110, -95, -100)
+  
+// Body
+  
+  fill(colorBody)
+  ellipse(0, -100, 240, 120)
+  
+// Eye
+  
+  fill(255)
+  ellipse(70, -110, 44)
+  fill(0)
+  ellipse(70, -110, 20)
+  
+// Body Fin
+  
+  fill(colorBodyFin)
+  noStroke()
+  arc(0, -100, 40, 40, 5*QUARTER_PI, PI - QUARTER_PI, PIE);
+
+   pop();
+  
+}
